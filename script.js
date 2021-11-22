5a3c74f5d6a6f0d944ddd2b24ca4c9692fa68bdd
Taylor Kolkmeyer <tkolkmeyer@gmail.com>
2021-11-22 16:47:11 -0600

    asdf

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 window.addEventListener('DOMContentLoaded', (event) => {
 
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
-        console.log(response);
+        response.json().then(function(json) {
+            const div = document.getElementById("container");
+            div.innerHTML = `
+                <h3>${json.firstName}</h3>
+                <ul>
+                    <li>Hours in space: ${json.hoursInSpace}</li>
+                    <li>Active: ${json.active}</li>
+                    <li>Skills: ${json.skills}</li>
+                </ul>
+                <img class="avatar" src="${json.picture}">
+                  `;
+        });
     });
 });
